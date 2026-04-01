4af52932b5fe272a652674fa7b3ef84e76020c3c
unknown <avargas@infosgroup.com>
2026-04-01 10:44:05 -0500

    test

---
+++ testPython.py
@@ -0,0 +1,28 @@
+def calculate_average(numbers):
+    # Empiece a escribir aquí y observe cómo Copilot sugiere la función body.
+
+    #quiero sumar dos numeros e imprimer el resultado
+    resultado = numbers[0] + numbers[1] 
+    print(resultado)
+# Ejemplo de uso
+calculate_average([10, 20]) 
+
+def calculate_average(numbers):
+    return sum(numbers) / len(numbers)
+
+print(calculate_average([10, 20]))
+
+#ahora multiplica dos numeros e imprimer el resultado
+def multiply_numbers(a, b):
+    resultado = a * b
+    print(resultado) 
+
+multiply_numbers(5, 4)
+
+def sumar_y_multiplicar(a, b):
+    #la suma es de la variable c y d
+    c = a + b
+    d = a * b
+    print(f"La suma es: {c} y la multiplicación es: {d}")
+
+#dividir dos numeros e imprimer el resultado
